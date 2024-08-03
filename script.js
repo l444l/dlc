2ad9841a35462cefb32fff2ee1dc994c21600516
l444l <153912295+l444l@users.noreply.github.com>
2024-08-03 14:11:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const resultsContainer = document.getElementById('results-container');
